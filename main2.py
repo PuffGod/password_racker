b8c6f48ca03603014435d6839420aca1c04166ab
PuffGod <147107589+PuffGod@users.noreply.github.com>
2023-10-12 14:22:14 -0700

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,16 @@
+import itertools
+import string
+import time
+
+#define all characters to use in the password
+chars = string.printable
+
+#define all characters to use in the password
+password="password123"
+
+# define the maximum password length to try
+ max_length = 10
+
+#track the start time of the password cracking process
+start_time = time.time()
+
