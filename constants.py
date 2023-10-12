e0eeb7f86afdefa49db91f973ff0775f9757a0e4
Brent Allen <carvetighter@gmail.com>
2023-10-12 17:48:31 -0500

    added logger and other utilties

---
+++ constants.py
@@ -0,0 +1,10 @@
+'''
+<file doc string>
+'''
+
+'''
+datetime constants
+'''
+
+STRING_DT_FORMAT = '%Y-%m-%d'
+INT_DT_FORMAT = '%Y%m%d'
--- main.py
+++ main.py
@@ -1,5 +1,47 @@
+'''
+<file doc string>
+'''
 
+'''
+packages
+'''
 
+from datetime import datetime
+from time import perf_counter
+from typing import List
+
+from utilities import set_up_logger
+from constants import STRING_DT_FORMAT
+
+'''
+logger
+'''
+
+import logging
+string_dt_now = datetime.now().strftime(STRING_DT_FORMAT)
+logger = logging.getLogger()
+logger.name = 'cracker'
+log_file = f'cracker_{string_dt_now}.log'
+log_path = './logs'
+set_up_logger(log_path, log_file, logger)
+
+'''
+supportive functions
+'''
+
+'''
+callable functions
+'''
+
+def create_passwords(number_to_create:int) -> List[str]:
+    '''
+    '''
+
+    return None
+
+'''
+main function
+'''
 
 def main():
     '''
