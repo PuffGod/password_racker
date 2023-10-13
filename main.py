56ed95947a84e16d2ea56b0893d4ec9e30da4997
Brent Allen <carvetighter@gmail.com>
2023-10-12 19:45:48 -0500

    finished password cracker two ways

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ packages
 import string
 import random
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from time import perf_counter
 from typing import List, Tuple, Generator
 from itertools import product
@@ -96,22 +96,94 @@ def create_passwords(number_to_create:int, password_max_length:int, characters_t
 
     return passwords_to_crack
 
-def crack_password(password:str, characters_to_use:str) -> Tuple[bool, str, float]:
+def crack_password_brute_force(password:str, characters_to_use:str) -> Tuple[bool, str, float, int]:
     '''
+    uses brute for method to crack password
+
+    :param str password: password to crack
+    :param str characters_to_use: ascii characters to use
+    :return: flag if fournd, the original password and the time to crack in seconds
+        tuple[0] -> bool; True if found, False if not found
+        tuple[1] -> string; original password
+        tuple[2] -> float; time to crack in seconds
+        tuple[3] -> int; number of guesses used
+    :rtype: tuple
     '''
-    number_characters_in_password = [9, 10, 11, 12]
+    # set-up
+    bool_return = False
 
-    potential_passwords = _gen_all_passwords(password_length = 2, characters = characters_to_use)
+    # generate all passowrds
+    potential_passwords = _gen_all_passwords(
+        password_length = len(password), characters = characters_to_use
+    )
+    logger.info('generated all potential passwords')
 
-    counter = 0
+    # brute force crack
+    logger.info('starting to crack password')
+    start_time = perf_counter()
+    counter = 1
     for pt_pswd in potential_passwords:
-        print(''.join(pt_pswd))
+        # get string from tuple
+        pwd = ''.join(pt_pswd)
 
-        if counter >= 5:
+        # compare password
+        if pwd == password:
+            stop_time = perf_counter()
+            bool_return = True
             break
+        
         counter += 1
 
-    return False, 'TSMA', 0.4
+    logger.info('finished cracking password')
+
+    return bool_return, password, stop_time - start_time, counter
+
+def crack_password_random_selection(password:str, characters_to_use:str, max_time:int) -> Tuple[bool, str, float, int]:
+    '''
+    uses random guess to attempt to crack password; if not cracked in perscribed time give up and get new
+    password
+
+    :param str password: password to crack
+    :param str characters_to_use: ascii characters to use
+    :param int max_time: maximum allowable time, in minutes, to try to crack a password
+    :return: flag if fournd, the original password and the time to crack in seconds
+        tuple[0] -> bool; True if found, False if not found
+        tuple[1] -> string; original password
+        tuple[2] -> float; time to crack in seconds
+        tuple[2] -> int; guess count
+    :rtype: tuple
+    '''
+    # set-up
+    bool_return = False
+    td_max_time = timedelta(minutes = max_time)
+    dt_start = datetime.now()
+    keep_guessing = True
+    
+    # start guessing
+    logger.info('starting to guess using random selection')
+    start_time = perf_counter()
+    counter = 1
+
+    while datetime.now() - dt_start < td_max_time and keep_guessing:
+        password_guess = _gen_random_password(
+            password_length = len(password),
+            characters = characters_to_use
+        )
+
+        # test if guessed correct
+        if password_guess == password:
+            stop_time = perf_counter()
+            bool_return = True
+            keep_guessing = False
+            logger.info('found password by random guessing')
+            break
+
+        counter += 1
+    
+    if not bool_return:
+        stop_time = perf_counter()
+
+    return bool_return, password, stop_time - start_time, counter
 
 '''
 main function
@@ -130,15 +202,23 @@ def main():
         characters_to_use = characters_to_use
     )
 
-    # crack password
-
-    # for password in passwords_to_crack:
-    found, found_password, time_to_crack = crack_password(
+    # crack password using brute force
+    found, found_password, time_to_crack, guess_count = crack_password_brute_force(
         password = '12345',
         characters_to_use = characters_to_use
     )
+
+    # crack password through random guessing
+    found, found_password, time_to_crack, guess_count = crack_password_random_selection(
+        password = '12',
+        characters_to_use = characters_to_use,
+        max_time = 1
+    )
+
     if found:
-        logger.info(f'found password -> {found_password} in {time_to_crack}')
+        logger.info(f'found password -> {found_password} in {time_to_crack:5f} seconds used {guess_count:,} guesses')
+    else:
+        logger.error(f'did not find password -> {found_password} in {time_to_crack:5f} seconds used {guess_count:,} guesses')
 
     return None
 
